3d7be866607ad32b76f523821c884a544546f999
Marcondes Cunha Araujo <marcunharaujo@gmail.com>
2021-09-18 12:33:10 -0300

    Avarage Grade Calculator

---
+++ AverageGradeCalculator.php
@@ -0,0 +1,40 @@
+<?php
+
+class AverageGradeCalculator{
+
+    public array $grades;
+    public int $grades1;
+
+    public function __construct(array $gradesParam) {
+
+        $this->grades = $gradesParam;
+
+    }
+
+    private function countGrades(): int{
+    
+        return sizeof($this->grades);
+        
+    }
+
+    private function addGrades(): int{
+    
+        $sumOfGrades = 0;
+
+        for ($i=0; $i < $this->countGrades(); $i++) { 
+
+            $sumOfGrades += $this->grades[$i];
+
+        }
+
+        return $sumOfGrades;
+
+    }
+
+    public function calculateGradeAverage(){
+
+        return $this->addGrades() / $this->countGrades();
+        
+    }
+
+}
